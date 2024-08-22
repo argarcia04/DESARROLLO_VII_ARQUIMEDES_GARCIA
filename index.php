6cda2ea1620274b542f134b2b60d3288dc994926
argarcia04 <garquimedes2@gmaill.com>
2024-08-21 19:36:25 -0500

    Agregada concatenacion de variables y constantes

--- index.php
+++ index.php
@@ -71,6 +71,26 @@
             echo "Altura: $altura<br>";
           echo "¿Es estudiante? " . ($esEstudiante ? "Sí" : "No");
 
+          $nombre = "Juan";
+          $edad = 25;
+
+          // Concatenación usando el operador .
+          $presentacion1 = "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años.";
+
+          // Concatenación dentro de comillas dobles
+          $presentacion2 = "Hola, mi nombre es $nombre y tengo $edad años.";
+
+          // Definición de una constante
+          define("SALUDO", "¡Bienvenido!");
+
+          // Concatenación con constante
+          $mensaje = SALUDO . " " . $nombre;
+
+          echo $presentacion1 . "<br>";
+          echo $presentacion2 . "<br>";
+          echo $mensaje . "<br>";
+
+
           ?>
                         
 
