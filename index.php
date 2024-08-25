3a07c7884cb9023a1dc12aea9eb3a28f68838f65
argarcia04 <garquimedes2@gmaill.com>
2024-08-25 14:55:37 -0500

    git commit -m "Agregados ejemplos de impresión en PHP"

--- index.php
+++ index.php
@@ -89,7 +89,24 @@
           echo $presentacion1 . "<br>";
           echo $presentacion2 . "<br>";
           echo $mensaje . "<br>";
+         
+          $nombre = "Juan";
+          $edad = 25;
 
+          // Usando echo
+          echo "Hola, mundo!<br>";
+          echo "Mi nombre es $nombre<br>";
+
+          // Usando print
+          print "Tengo $edad años<br>";
+
+        // Usando printf (permite formateo)
+        printf("Me llamo %s y tengo %d años<br>", $nombre, $edad);
+
+        // Usando var_dump (útil para debugging)
+        var_dump($nombre);
+        echo "<br>";
+          
 
           ?>
                         
